b22a80511c507de7c4f9efd32138c369aff940ef
Patrick Schmidt <patrick.schmidt@student.hpi.uni-potsdam.de>
2013-12-05 16:09:39 +0100

    implementing sockets

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,7 @@
 #include "LaserRectangle.h"
 #include "LaserCircle.h"
 #include "LaserLine.h"
+#include "LaserServer.h"
 
 #include <chrono>
 
@@ -11,6 +12,7 @@
 #include <cmath>
 
 #include <vector>
+#include <iostream>
 
 using namespace laser;
 
@@ -20,19 +22,11 @@ int main(void)
 
 	p.aquireEtherdreamWrapper();
 
-	LaserObjectPtr c(new LaserRectangle(-5000, -2500, 10000, 5000));
-	LaserObjectPtr d(new LaserCircle(0, 0, 10000));
-	LaserObjectPtr f(new LaserLine(-10000, 10000, 10000, -10000));
-	p.add(c);
-	p.add(d);
-	//p.add(f);
+	LaserServer s(p);
 
-	while (true)
-	{
-		c->rotate(M_PI/100);
-		p.updatePoints();
-		std::this_thread::sleep_for(std::chrono::milliseconds(5));
-	}
+	s.poll();
+
+	std::cout << "juhu" << std::endl;
 
 	return 0;
 }
