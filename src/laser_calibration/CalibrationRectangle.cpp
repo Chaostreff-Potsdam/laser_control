99bd733a00f032e1813860fa5566f43874477f5e
Hannes Rantzsch <hannes.rantzsch@student.hpi.uni-potsdam.de>
2013-12-18 16:47:21 +0100

    laser_configuration

---
+++ src/laser_calibration/CalibrationRectangle.cpp
@@ -0,0 +1,69 @@
+#include "CalibrationRectangle.h"
+
+#include <iostream>
+#include <cstdint>
+
+CalibrationRectangle::CalibrationRectangle(cv::Point2f topLeft, cv::Point2f bottomLeft, cv::Point2f bottomRight, cv::Point2f topRight)
+    : m_topLeft(topLeft), m_bottomLeft(bottomLeft), m_bottomRight(bottomRight), m_topRight(topRight)
+{
+}
+
+std::vector<etherdream_point> CalibrationRectangle::points()
+{
+    std::vector<etherdream_point> points;
+
+    float pointsPerLine = 50;
+
+    // topLeft - bottomLeft
+    cv::Point2f fromTo(m_bottomLeft.x - m_topLeft.x, m_bottomLeft.y - m_topLeft.y);
+    for (int i = 0; i < pointsPerLine; ++i)
+    {
+        cv::Point2f newPoint(m_topLeft.x + (i/pointsPerLine) * fromTo.x, m_topLeft.y + (i/pointsPerLine) * fromTo.y);
+        points.push_back(etherdream_point { newPoint.x, newPoint.y, 0, UINT16_MAX, 0 } );
+    }
+
+    // bottomLeft - bottomRight
+    fromTo = cv::Point2f(m_bottomRight.x - m_bottomLeft.x, m_bottomRight.y - m_bottomLeft.y);
+    for (int i = 0; i < pointsPerLine; ++i)
+    {
+        cv::Point2f newPoint(m_bottomLeft.x + (i/pointsPerLine) * fromTo.x, m_bottomLeft.y + (i/pointsPerLine) * fromTo.y);
+        points.push_back(etherdream_point { newPoint.x, newPoint.y, 0, UINT16_MAX, 0 } );
+    }
+
+    // bottomRight - topRight
+    fromTo = cv::Point2f(m_topRight.x - m_bottomRight.x, m_topRight.y - m_bottomRight.y);
+    for (int i = 0; i < pointsPerLine; ++i)
+    {
+        cv::Point2f newPoint(m_bottomRight.x + (i/pointsPerLine) * fromTo.x, m_bottomRight.y + (i/pointsPerLine) * fromTo.y);
+        points.push_back(etherdream_point { newPoint.x, newPoint.y, 0, UINT16_MAX, 0 } );
+    }
+
+    // topRight - topLeft
+    fromTo = cv::Point2f(m_topLeft.x - m_topRight.x, m_topLeft.y - m_topRight.y);
+    for (int i = 0; i < pointsPerLine; ++i)
+    {
+        cv::Point2f newPoint(m_topRight.x + (i/pointsPerLine) * fromTo.x, m_topRight.y + (i/pointsPerLine) * fromTo.y);
+        points.push_back(etherdream_point { newPoint.x, newPoint.y, 0, UINT16_MAX, 0 } );
+    }
+
+    return points;
+}
+
+std::vector<cv::Point2f> CalibrationRectangle::corners()
+{
+    std::vector<cv::Point2f> corners;
+    corners.push_back(m_topLeft);
+    corners.push_back(m_bottomLeft);
+    corners.push_back(m_bottomRight);
+    corners.push_back(m_topRight);
+
+    return corners;
+}
+
+void CalibrationRectangle::print()
+{
+    std::cout << "(" << m_topLeft.x << "|" << m_topLeft.y << ") \t" <<
+                 "(" << m_topRight.x << "|" << m_topRight.y << ")" << std::endl <<
+                 "(" << m_bottomLeft.x << "|" << m_bottomLeft.y << ") \t" <<
+                 "(" << m_bottomRight.x << "|" << m_bottomRight.y << ")" << std::endl;
+}
---
+++ src/laser_calibration/EtherdreamWrapper.cpp
@@ -0,0 +1,100 @@
+#include "EtherdreamWrapper.h"
+
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+#include <iostream>
+
+#include "etherdream.h"
+
+using namespace laser;
+
+EtherdreamWrapper::EtherdreamWrapper()
+{
+	m_newPoints = false;
+	connect();
+}
+
+EtherdreamWrapper::~EtherdreamWrapper()
+{
+	if(m_etherdream)
+		etherdream_disconnect(m_etherdream);
+}
+
+bool EtherdreamWrapper::empty()
+{
+	std::lock_guard<std::mutex> guard(m_pointsMutex);
+
+	return m_points.empty();
+}
+
+void EtherdreamWrapper::clear()
+{
+	std::lock_guard<std::mutex> guard(m_pointsMutex);
+
+	m_points.clear();
+}
+
+void EtherdreamWrapper::connect()
+{
+	try
+	{
+		// start listening for etherdreams
+		etherdream_lib_start();
+
+		// this will take atleast a second
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(1200));
+
+		// how many have we seen
+		int etherdream_count = etherdream_dac_count();
+
+		// if none, stop
+		if (!etherdream_count)
+		{
+			return;
+		}
+
+		// we will always take the first
+		m_etherdream = etherdream_get(0);
+
+		// establish TCP connection
+		etherdream_connect(m_etherdream);
+
+		return;
+	}
+	catch (...) // if the program wants us to stop
+	{
+		if(m_etherdream)
+			etherdream_disconnect(m_etherdream);
+		return;
+	}
+}
+
+void EtherdreamWrapper::writePoints()
+{
+	std::lock_guard<std::mutex> guard(m_pointsMutex);
+
+//	std::cout << "a" << std::endl;
+	etherdream_write(m_etherdream, m_points.data(), m_points.size(), 30000, -1);
+	m_newPoints = false;
+}
+
+void EtherdreamWrapper::setPoints(std::vector<etherdream_point> &p)
+{
+//	std::cout << "setPoints" << std::endl;
+	std::lock_guard<std::mutex> guard(m_pointsMutex);
+
+	m_points = p;
+	m_newPoints = true;
+}
+
+void EtherdreamWrapper::addPoints(const std::vector<etherdream_point> &p)
+{
+//	std::cout << "addPoints" << std::endl;
+	std::lock_guard<std::mutex> guard(m_pointsMutex);
+
+	m_points.insert(m_points.end(), p.begin(), p.end());
+	m_newPoints = true;
+}
