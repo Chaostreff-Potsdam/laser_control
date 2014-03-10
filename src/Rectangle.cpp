461ff170d532c0fa1640d573bc4b46bb977b558c
Florian Meinel <florian.meinel@student.hpi.uni-potsdam.de>
2014-03-10 12:09:19 +0100

    additional rectangle constructor, taking eight ints

--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -23,3 +23,13 @@ laser::Rectangle::Rectangle(Point p1, Point p2, Point p3, Point p4, bool sharp)
 	m_corners.push_back(p3);
 	m_corners.push_back(p4);
 }
+
+laser::Rectangle::Rectangle(int p1x, int p1y, int p2x, int p2y, int p3x, int p3y, int p4x, int p4y, bool sharp)
+:	Polygon(sharp)
+{
+	m_corners.push_back(Point(p1x, p1y));
+	m_corners.push_back(Point(p2x, p2y));
+	m_corners.push_back(Point(p3x, p3y));
+	m_corners.push_back(Point(p4x, p4y));
+}
+
--- src/Rectangle.h
+++ src/Rectangle.h
@@ -11,6 +11,7 @@ namespace laser {
 	public:
 		Rectangle(int x, int y, int width, int height, bool sharp = true, double rotation = 0);
 		Rectangle(Point p1, Point p2, Point p3, Point p4, bool sharp = true);
+		Rectangle(int p1x, int p1y, int p2x, int p2y, int p3x, int p3y, int p4x, int p4y, bool sharp);
 	};
 }
 
