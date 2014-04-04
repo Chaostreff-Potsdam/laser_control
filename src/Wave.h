d879b810daa3928f9e96920371c0852d6a68cddc
Patrick Schmidt <patrick.schmidt@student.hpi.uni-potsdam.de>
2014-04-04 15:34:39 +0200

    introduced new prototypical sine wave

---
+++ src/Wave.h
@@ -0,0 +1,27 @@
+#ifndef LASERWAVE_H
+#define LASERWAVE_H
+
+#include "Object.h"
+
+namespace laser {
+	class EXPORT_LASER_CONTROL Wave : public Object
+	{
+	public:
+		Wave(Point p1, Point p2, int amplitude, int frequency);
+
+		EtherdreamPoints points() const;
+		EtherdreamPoints startPoints() const;
+		EtherdreamPoints endPoints() const;
+
+	protected:
+		Point m_p1;
+		Point m_p2;
+
+		int m_amplitude;
+		int m_frequency;
+		int m_pointCount;
+	};
+}
+
+
+#endif // LASERWAVE_H
