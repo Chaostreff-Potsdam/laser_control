cb1945328a8de9d40423dea2f669f3415832e4b9
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2014-04-14 16:51:05 +0200

    Fallback code for MSVC and other old compilers

--- src/holodeck/InstructionCaller.h
+++ src/holodeck/InstructionCaller.h
@@ -1,7 +1,61 @@
 #pragma once
 #include "../Painter.h"
 
+#include <boost/config.hpp>
+
 namespace laser { namespace holodeck {
+
+#ifdef BOOST_NO_CXX11_VARIADIC_TEMPLATES
+
+template <size_t num_args>
+struct InstructionCaller;
+
+template<>
+struct InstructionCaller<1>
+{
+	template <typename FuncType>
+	ObjectPtr operator() (FuncType f, const std::vector<Point> &ps) {
+		return f(ps[0]);
+	}
+};
+
+template<>
+struct InstructionCaller<2>
+{
+	template <typename FuncType>
+	ObjectPtr operator() (FuncType f, const std::vector<Point> &ps) {
+		return f(ps[0], ps[1]);
+	}
+};
+
+template<>
+struct InstructionCaller<3>
+{
+	template <typename FuncType>
+	ObjectPtr operator() (FuncType f, const std::vector<Point> &ps) {
+		return f(ps[0], ps[1], ps[2]);
+	}
+};
+
+template<>
+struct InstructionCaller<4>
+{
+	template <typename FuncType>
+	ObjectPtr operator() (FuncType f, const std::vector<Point> &ps) {
+		return f(ps[0], ps[1], ps[2], ps[3]);
+	}
+};
+
+template<>
+struct InstructionCaller<5>
+{
+	template <typename FuncType>
+	ObjectPtr operator() (FuncType f, const std::vector<Point> &ps) {
+		return f(ps[0], ps[1], ps[2], ps[3], ps[4]);
+	}
+};
+
+#else
 
 template <std::size_t... Indices>
 struct indices {
@@ -34,5 +88,7 @@ private:
 		return (*f)(args[I]...);
 	}
 };
+
+#endif
 
 }} // namespace laser::holodeck
