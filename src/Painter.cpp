5d38bfc2928041194ba83596666d2d8363843c65
Hannes Rantzsch <hannes.rantzsch@student.hpi.uni-potsdam.de>
2014-05-21 14:09:07 +0200

    Merged stash, fixed NOT READY, DeleteAll broken again

--- src/Painter.cpp
+++ src/Painter.cpp
@@ -127,6 +127,11 @@ void laser::Painter::removeExpiredObjects()
 
 void laser::Painter::updatePoints()
 {
+	#ifndef _WIN32
+	if (m_objects.empty())
+		return;
+	#endif
+
 	EtherdreamPoints ps;
 
 	{
