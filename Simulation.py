4841bb2ef70808095b61e0e2643ee199231c4437
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2014-07-11 16:33:45 +0200

    Adapt Python to UDP-JSON protocol, generate instruction codes from C++ file, drop in interactive shell

--- Simulation.py
+++ Simulation.py
@@ -1,31 +1,16 @@
+#!/usr/bin/env python -i
 import struct
 import socket
 import itertools
+import json
+import getHolodeckEnum
 
 """
 A point is a (int, int)-tuple
 """
 
-Delete = 1
-DeleteAll = 2
-Player = 3
-Wall = 4
-Door = 5
-Table = 6
-Button = 7
-Beam = 8
-PortalInactive = 9
-PortalActive = 10
-Zipline = 11
-Corpse = 12
-Stool = 13
-Water = 14
-Poke = 15
-Stomper = 16
-Footwear = 17
-Heat = 18
-Elevator = 19
-Guardrail = 20
+globals().update(dict(
+	(handler, idx) for idx, handler in enumerate(getHolodeckEnum.findHandlers())))
 
 class LaserClient(object):
 
@@ -38,15 +23,22 @@ class LaserClient(object):
 	def __del__(self):
 		self.socket.close()
 
-	def __sendPacket(self, fmt_string, *values):
-		packet = struct.pack(fmt_string, *values)
-		self.socket.sendto(packet, (self.host, self.port))
+	def __sendPacket(self, code, instructionID=None, other=None):
+		packet = other.copy() if other is not None else {}
+		packet.update({"instruction": code})
+		
+		if instructionID is not None:
+			packet.update({"id": instructionID})
+		self.socket.sendto(json.dumps(packet), (self.host, self.port))
+
+	def __packPoints(self, points):
+		return [{"x": x, "y": y} for x, y in points]
 
 	def delete(self, instructionID):
-		self.__sendPacket("<BI", Delete, instructionID)
+		self.__sendPacket(Delete, instructionID)
 
 	def deleteAll(self):
-		self.__sendPacket("<B", DeleteAll)
+		self.__sendPacket(DeleteAll)
 
 	def instruction(self, code, points=None, instructionID=-1, turkerIds=None):
 		if points is None:
@@ -56,9 +48,27 @@ class LaserClient(object):
 		if instructionID == -1:
 			self.lastSendID += 1
 			instructionID = self.lastSendID
+		if turkerIds is None:
+			turkerIds = []
 
-		self.__sendPacket("<BIi" + "i" * 2 * len(points),
-				code, instructionID, 0,
-				 *itertools.chain(*points))
+		self.__sendPacket(code, instructionID, 
+				{"points": self.__packPoints(points),
+				 "turkers": turkerIds})
 		return instructionID
 
+def interact(host, port):
+	globals()["laser"] = LaserClient(host, port)
+	print """Created a UDP connection to %s:%d with object named \"laser\"
+Usage:
+\tlaser.deleteAll()
+\tplayerID = laser.instruction(Player, [(100, 100)])
+\tlaser.instruction(Player, [(1000, 1000)], playerID)""" % (host, port)
+
+if __name__ == "__main__":
+	import sys
+	if sys.flags.inspect:
+		host = sys.argv[1] if len(sys.argv) > 1 else "localhost"
+		port = int(sys.argv[2]) if len(sys.argv) > 2 else 30000
+		interact(host, port)
+	else:
+		print "Execute with \"python -i %s [host [port]]\"" % sys.argv[0]
