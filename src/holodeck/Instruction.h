962dbecfcce5398f1ec2b12cc70f41522f3e7370
Patrick Schmidt <patrick.schmidt@student.hpi.uni-potsdam.de>
2014-08-04 14:43:23 +0200

    moved digit shit to instruction factory. numbers in middle?

--- src/holodeck/Instruction.h
+++ src/holodeck/Instruction.h
@@ -13,26 +13,17 @@ namespace laser {
 		class Instruction;
 		typedef std::shared_ptr<Instruction> InstructionPtr;
 
-		typedef std::vector<std::vector<Point>> PointLUT;
 
 		class Instruction : public laser::CompositeObject
 		{
 		public:
-			static LASER_DEPRECATED InstructionPtr construct(const std::vector<ObjectPtr> & objects, int instructionId, const std::vector<int> & turkerIds);
-			static LASER_DEPRECATED InstructionPtr construct(const ObjectPtr object, int instructionId, const std::vector<int> & turkerIds);
 			static InstructionPtr construct(const Json::Value &root);
-
-			void putTurkerIdsAt(Point p);
 
 		protected:
 			Instruction(int instructionId, const std::vector<int> & turkerIds);
 
-			std::vector<ObjectPtr> turkerIdsToPolygons();
-
 			int m_instructionId;
 			std::vector<int> m_turkerIds;
-
-			static const PointLUT s_numbers;
 		};
 	}
 }
