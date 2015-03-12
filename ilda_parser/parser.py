21bfa65f400e83a1a02738c56c1a3ffd21dc48dd
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2015-03-12 14:41:59 +0100

    Write little parser for ILDA files

---
+++ ilda_parser/parser.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python2
+
+import struct
+
+class LaserPoint(object):
+	
+	def __init__(self, x, y, state, color):
+		self.x = x
+		self.y = y
+		self.state = state
+		self.color = color
+
+	def __repr__(self):
+		return "LaserPoint(%d, %d, %d, %d)" % (self.x, self.y, self.state, self.color)
+
+class ILDA(object):
+	
+	TYPE_3D = 0
+	TYPE_2D = 1
+	TYPE_COLOR_PALETTE = 2
+
+	headerFmt = ">4sxxxB8s8sHHHBx"
+	Point2DFmt = ">hhbb"
+	Point3DFmt = ">hhhbb"
+
+	def __init__(self, data):
+		self.readHeader(data[:32])
+		self.data = data[32:]
+
+	def correctString(self, s):
+		return s.partition("\s")[0]
+
+	def readHeader(self, headerData):
+		self.signature, self.formatT, \
+		self.name, self.company_name, self.dataCount, \
+		self.frameId, self.frameCount, self.scannerHead \
+				= struct.unpack(self.headerFmt, headerData)
+
+		assert self.signature == "ILDA"
+		assert self.formatT == self.TYPE_2D, "Can only parse 2D coordinates"
+		self.name = self.correctString(self.name)
+		self.company_name = self.correctString(self.company_name)
+
+	def __iter__(self):
+		return self.readPoints()
+
+	def readPoints(self):
+		pointDataSz = struct.calcsize(self.Point2DFmt)
+		assert pointDataSz * self.dataCount <= len(self.data)
+		for i in xrange(self.dataCount):
+			pointData = self.data[i * pointDataSz:(i + 1) * pointDataSz]
+			point = LaserPoint(*struct.unpack(self.Point2DFmt, pointData))
+			yield point
+
+	def leftData(self):
+		return self.data[pointDataSz * self.dataCount:]
+		
+	def hasDataLeft(self):
+		return bool(leftData)
+
+def main(filename):
+	doc = ILDA(open(filename).read())
+	for p in doc:
+		print p
+
+if __name__ == "__main__":
+	import sys
+	if len(sys.argv) > 1:
+		main(sys.argv[1])
+	else:
+		print "Usage: %s" % sys.argv[0]
+
